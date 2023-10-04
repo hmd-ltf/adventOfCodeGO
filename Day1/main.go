5c788f1dba07d80e5ad9c6eeda25af954a689b43
hmd <mhmd.ltf786@gmail.com>
2023-10-05 00:47:01 +0500

    removed some warnings

--- Day1/main.go
+++ Day1/main.go
@@ -8,68 +8,73 @@ import (
 	"strings"
 )
 
-func main()  {
-  calories := readFileData()
-  caloriesPerPerson := sumAllCalories(calories)
-  personCalories := findMaxOfTopThreeElfCalories(caloriesPerPerson)
-  fmt.Println(personCalories)
+func main() {
+	calories := readFileData()
+	caloriesPerPerson := sumAllCalories(calories)
+	personCalories := findMaxOfTopThreeElfCalories(caloriesPerPerson)
+	fmt.Println(personCalories)
 }
 
 func sumAllCalories(calories []string) []int {
-  personalCaloriesCount := []int{0}
-  personalCaloriesIndex := 0
+	personalCaloriesCount := []int{0}
+	personalCaloriesIndex := 0
 
-  for _, value := range calories {
-    if (len(value) != 0) {
-      num, err := strconv.Atoi(value)
+	for _, value := range calories {
+		if len(value) != 0 {
+			num, err := strconv.Atoi(value)
 
-      if (err != nil) {
-        fmt.Println("Unable to parse string", err)
-      } else {
-        personalCaloriesCount[personalCaloriesIndex] = personalCaloriesCount[personalCaloriesIndex] + num
-      }
-    } else {
-      personalCaloriesIndex++
-      personalCaloriesCount = append(personalCaloriesCount, 0)
-    }
-  }
+			if err != nil {
+				fmt.Println("Unable to parse string", err)
+			} else {
+				personalCaloriesCount[personalCaloriesIndex] = personalCaloriesCount[personalCaloriesIndex] + num
+			}
+		} else {
+			personalCaloriesIndex++
+			personalCaloriesCount = append(personalCaloriesCount, 0)
+		}
+	}
 
-  return personalCaloriesCount
+	return personalCaloriesCount
 }
 
 func readFileData() []string {
-  var caloriesData []string
-  file, _ := os.Open("data.txt")
-  defer file.Close()
+	var caloriesData []string
+	file, _ := os.Open("data.txt")
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			print("unable to close the file due to: ", err)
+		}
+	}(file)
 
-  scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-  for scanner.Scan() {
-    line := scanner.Text()
-    caloriesData = append(caloriesData, strings.Join(strings.Split(line, " "), ""))
-  }
+	for scanner.Scan() {
+		line := scanner.Text()
+		caloriesData = append(caloriesData, strings.Join(strings.Split(line, " "), ""))
+	}
 
-  return caloriesData
+	return caloriesData
 }
 
 func findMaxOfTopThreeElfCalories(summedCalories []int) int {
-  mostCaloriesByFirstElf := 0
-  mostCaloriesBySecondElf := 0
-  mostCaloriesByThirdElf := 0
+	mostCaloriesByFirstElf := 0
+	mostCaloriesBySecondElf := 0
+	mostCaloriesByThirdElf := 0
 
-  for _, value := range summedCalories {
+	for _, value := range summedCalories {
 
-    if (mostCaloriesByFirstElf < value) {
-      mostCaloriesByThirdElf = mostCaloriesBySecondElf
-      mostCaloriesBySecondElf = mostCaloriesByFirstElf
-      mostCaloriesByFirstElf = value
-    } else if (mostCaloriesBySecondElf < value) {
-      mostCaloriesByThirdElf = mostCaloriesBySecondElf
-      mostCaloriesBySecondElf = value
-    } else if (mostCaloriesByThirdElf < value) {
-      mostCaloriesByThirdElf = value
-    }
-  }
+		if mostCaloriesByFirstElf < value {
+			mostCaloriesByThirdElf = mostCaloriesBySecondElf
+			mostCaloriesBySecondElf = mostCaloriesByFirstElf
+			mostCaloriesByFirstElf = value
+		} else if mostCaloriesBySecondElf < value {
+			mostCaloriesByThirdElf = mostCaloriesBySecondElf
+			mostCaloriesBySecondElf = value
+		} else if mostCaloriesByThirdElf < value {
+			mostCaloriesByThirdElf = value
+		}
+	}
 
-  return mostCaloriesByFirstElf + mostCaloriesBySecondElf + mostCaloriesByThirdElf
+	return mostCaloriesByFirstElf + mostCaloriesBySecondElf + mostCaloriesByThirdElf
 }
--- Day2/main.go
+++ Day2/main.go
@@ -7,100 +7,103 @@ import (
 	"strings"
 )
 
-func main()  {
-  roundDetails := readFileData()
-  totalScore := calculateScore(roundDetails)
-
-  print(totalScore)
+func main() {
+	roundDetails := readFileData()
+	totalScore := calculateScore(roundDetails)
+	print(totalScore)
 }
 
 func calculateScore(roundsData []string) int {
-  totalScore := 0
-  inputToScoreMap := make(map[string]int)
+	totalScore := 0
+	inputToScoreMap := make(map[string]int)
 
-  for _, value := range roundsData {
-    roundScore, err := findScore(value, inputToScoreMap)
+	for _, value := range roundsData {
+		roundScore, err := findScore(value, inputToScoreMap)
 
-    if (err == nil) {
-      totalScore += roundScore
-    }
-  }
-  
-  return totalScore
+		if err == nil {
+			totalScore += roundScore
+		}
+	}
+
+	return totalScore
 }
 
 func readFileData() []string {
-  var data []string
-  file, _ := os.Open("data.txt")
-  defer file.Close()
+	var data []string
+	file, _ := os.Open("data.txt")
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			print("unable to close the file due to: ", err)
+		}
+	}(file)
 
-  scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-  for scanner.Scan() {
-    line := scanner.Text()
-    data = append(data, line)
-  }
+	for scanner.Scan() {
+		line := scanner.Text()
+		data = append(data, line)
+	}
 
-  return data
+	return data
 }
 
 func findScore(roundDetails string, inputToScoreMap map[string]int) (int, error) {
-  score, scoreAlreadyCalculated:= inputToScoreMap[roundDetails]
+	score, scoreAlreadyCalculated := inputToScoreMap[roundDetails]
 
-  if (!scoreAlreadyCalculated) {
-    moves := strings.Split(roundDetails, " ")
+	if !scoreAlreadyCalculated {
+		moves := strings.Split(roundDetails, " ")
 
-    if (len(moves) == 2) {
-      opponentsMove := moves[0]
-      roundResult := moves[1]
-      var isUsersWin bool
-      var isDraw bool
+		if len(moves) == 2 {
+			opponentsMove := moves[0]
+			roundResult := moves[1]
+			var isUsersWin bool
+			var isDraw bool
 
-      if (roundResult == "X") {
-        isUsersWin = false
-        isDraw = false
-      } else if (roundResult == "Y") {
-        isUsersWin = false
-        isDraw = true
-        score += 3
-      } else {
-        isUsersWin = true
-        isDraw = false
-        score += 6
-      }
+			if roundResult == "X" {
+				isUsersWin = false
+				isDraw = false
+			} else if roundResult == "Y" {
+				isUsersWin = false
+				isDraw = true
+				score += 3
+			} else {
+				isUsersWin = true
+				isDraw = false
+				score += 6
+			}
 
-      if (opponentsMove == "A") {
-        if (isUsersWin) {
-          score += 2
-        } else if (isDraw) {
-          score += 1
-        } else {
-          score += 3
-        }
-      } else if (opponentsMove == "B") {
-        if (isUsersWin) {
-          score += 3
-        } else if (isDraw) {
-          score += 2
-        } else {
-          score += 1
-        }
+			if opponentsMove == "A" {
+				if isUsersWin {
+					score += 2
+				} else if isDraw {
+					score += 1
+				} else {
+					score += 3
+				}
+			} else if opponentsMove == "B" {
+				if isUsersWin {
+					score += 3
+				} else if isDraw {
+					score += 2
+				} else {
+					score += 1
+				}
+			} else {
+				if isUsersWin {
+					score += 1
+				} else if isDraw {
+					score += 3
+				} else {
+					score += 2
+				}
+			}
 
-      } else {
-        if (isUsersWin) {
-          score += 1
-        } else if (isDraw) {
-          score += 3
-        } else {
-          score += 2
-        }
-      }
+			inputToScoreMap[roundDetails] = score
+		} else {
+			return 0, errors.New("invalid turnDetails")
+		}
+	}
 
-      inputToScoreMap[roundDetails] = score
-    } else {
-      return 0, errors.New("Invalid turnDetails")
-    }
-  }
-
-  return score, nil
+	return score, nil
 }
