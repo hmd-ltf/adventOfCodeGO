bfb977ec0bbd721e99963936a8980ae078479b91
hmd <mhmd.ltf786@gmail.com>
2023-11-04 01:39:05 +0500

    day 4 read the file

--- Day4/main.go
+++ Day4/main.go
@@ -1,1 +1,61 @@
-package Day4
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type RangeStruct struct {
+	startingNumber int
+	endingNumber   int
+}
+
+type RangePairs struct {
+	firstPair  RangeStruct
+	secondPair RangeStruct
+}
+
+func main() {
+	rangePairs := readFileData()
+
+	print(rangePairs)
+}
+
+func readFileData() []RangePairs {
+	var rangePairs []RangePairs
+
+	file, _ := os.Open("data.txt")
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		rangePairs = append(rangePairs, prepareRangePairs(scanner.Text()))
+	}
+
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	return rangePairs
+}
+
+func prepareRangePairs(data string) RangePairs {
+	var rangePair RangePairs
+	ranges := strings.Split(data, ",")
+
+	rangePair.firstPair = prepareRangeStruct(ranges[0])
+	rangePair.secondPair = prepareRangeStruct(ranges[1])
+
+	return rangePair
+}
+
+func prepareRangeStruct(data string) RangeStruct {
+	var rangeStruct RangeStruct
+
+	ranges := strings.Split(data, "-")
+	rangeStruct.startingNumber, _ = strconv.Atoi(ranges[0])
+	rangeStruct.endingNumber, _ = strconv.Atoi(ranges[1])
+
+	return rangeStruct
+}
