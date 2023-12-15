24628ae3c42d7efcc9daa1111ddfa47035df28cd
Hammad Latif <mhmd.ltf786@gmail.com>
2023-12-15 09:35:50 +0000

    sosme progress

--- Day5/main.go
+++ Day5/main.go
@@ -1,5 +1,15 @@
 package main
 
+type moves struct {
+	moves     int
+	fromCrate rune
+	toCrate   rune
+}
+type crateStack struct {
+	label  rune
+	crates []rune
+}
+
 func main() {
 	print("Ready for day 5")
 }
